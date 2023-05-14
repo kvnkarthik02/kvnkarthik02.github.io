e98b78b954b5b83c1f9f002f4f884a78ba2ee2fd
Karthik Venkatesh <kvenkate@tcd.ie>
2023-05-15 00:30:40 +0400

    init

---
+++ personal-blog/src/components/Menu.svelte
@@ -0,0 +1,15 @@
+<div class="sidebar"> 
+    <ul>
+        <li>About Me</li>
+        <li> Work Experience</li>
+        <li>Projects Blog</li>
+    </ul>
+</div>
+
+<style>
+    .sidebar{
+        border-right: 1px solid black;
+        padding-right: 10px;
+        position: fixed;
+    }
+</style>
---
+++ personal-blog/src/routes/+page.svelte
@@ -0,0 +1,18 @@
+<script>
+    import Menu from '../components/Menu.svelte';
+</script>
+
+
+<h3 class = "header">Karthik Venkatesh Nagaraj</h3>
+
+<Menu />
+
+
+<style>
+    .header{
+        border-bottom: 1px solid black;
+        padding-bottom: 2px;
+    }
+
+    
+</style>
